4dc7298ff74790d92ced1ea9bcb4dfa4928da298
Winner <bihames4vainqueur@gmail.com>
2023-11-06 23:55:19 +0200

    add database connection

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 require('dotenv').config();
 
 const express = require('express')
+const mongoose = require('mongoose')
+const Book = require("./models/books");
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+//Routes go here
+app.get('/', (req,res) => {
+    res.send({ title: 'Books' });
+})
+
 app.get('/books', async (req,res)=> {
 
-  return res.json({
-    message: 'Got it',
-    data: [
+  const book = await Book.find();
+
+  if (book) {
+    res.json(book)
+  } else {
+    res.send("Something went wrong.");
+  }
+  
+});
+
+app.get('/add-note', async (req,res) => {
+  try {
+    await Book.insertMany([
       {
         title: "Sons Of Anarchy",
         body: "Body text goes here...",
@@ -18,10 +46,16 @@ app.get('/books', async (req,res)=> {
         title: "Games of Thrones",
         body: "Body text goes here...",
       }
-    ]
-  })
-});
+    ]);
+    res.json({"Data":"Added"})
+  } catch (error) {
+    console.log("err", + error);
+  }
+})
 
-app.listen(PORT, () => {
-  console.log("listening for requests: " + PORT);
-});
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
